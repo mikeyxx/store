bfce6802ac3ba4fbb1737aa22f3e916c4ab2f08c
mikeyxx <michaelogboo@gmail.com>
2023-04-23 19:30:42 +0100

    Adjusted the width on small and large screens

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -12,16 +12,18 @@ const Cards = ({ product, handleModal }: Props) => {
   const dispatch = useAppDispatch();
   return (
     <div
-      className="min-[1001px]:pt-[120px] min-[320px]:pt-[30px] cursor-pointer flex w-full items-center justify-center"
+      className="cursor-pointer flex items-center  min-[1001px]:pt-[89px] min-[380px]:pt-[20px] justify-center min-w-[1200px]:h-screen pt-24 pb-4"
       onClick={() => dispatch(addItemToPdp(product))}
     >
       <div className="added" onClick={handleModal}>
         <img src={product.img} alt="" />
-        <div>
+        <div className="mt-8">
           <p>{product.name}</p>
           <span>Price: {formatCurrency(product.price)}</span>
         </div>
-        <span className="underline text-secondary">Shop Now</span>
+        <span className="underline text-secondary font-bold block mt-7">
+          Shop Now
+        </span>
       </div>
     </div>
   );
